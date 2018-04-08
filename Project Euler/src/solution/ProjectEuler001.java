e3b0d66722ad1e4418587ab3027d51d728ca0d9d
Gumpy <franciszek.stepien@gmail.com>
2018-04-08 22:54:11 +0200

    Multiples of 3 and 5

--- Project Euler/src/solution/ProjectEuler001.java
+++ Project Euler/src/solution/ProjectEuler001.java
@@ -3,19 +3,19 @@ package solution;
 import java.util.Scanner;
 
 public class ProjectEuler001 {
-	static void sol(long number){
-        long b=(number-1)/3;
-        long c=(number-1)/5;
-        long d=(number-1)/15;
-        System.out.println(b*(b+1)/2*3+c*(c+1)/2*5-d*(d+1)/2*15);   
-    }
-	
-    public static void main(String[] args) {
+	public static void main(String[] args) {
         Scanner inputData = new Scanner(System.in);
         int casesNumber =inputData.nextInt();
         for(int i=0; i<casesNumber; i++){
             sol(inputData.nextLong());
         }
         inputData.close();
+    }
+	
+	static void sol(long number){
+        long b=(number-1)/3;
+        long c=(number-1)/5;
+        long d=(number-1)/15;
+        System.out.println(b*(b+1)/2*3+c*(c+1)/2*5-d*(d+1)/2*15);   
     }
 }
