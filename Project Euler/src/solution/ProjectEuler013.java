1fae096caf47e914d902c6ddd2aaad491c5b85f0
Gumpy <franciszek.stepien@gmail.com>
2018-06-11 18:55:21 +0200

    Large sum

--- Project Euler/src/solution/ProjectEuler013.java
+++ Project Euler/src/solution/ProjectEuler013.java
@@ -1,5 +1,23 @@
 package solution;
 
-public class ProjectEuler013 {
+import java.math.BigInteger;
+import java.util.Scanner;
 
+public class ProjectEuler013  {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int numbers = scanner.nextInt();
+		BigInteger sum = BigInteger.valueOf(0);
+		for(int i = 0; i < numbers; i++) {
+			sum = sum.add(new BigInteger(scanner.next()));
+		}
+		scanner.close();
+		printAnswer(sum);
+	}
+
+	private static void printAnswer(BigInteger sum) {
+		String number = sum.toString();
+		number = number.substring(0, 10);
+		System.out.println(number);
+	}
 }
